25569a1b775e86aaa66025f7d7732a0d33c27207
bladimir <b.rosario@sisalril.gob.do>
2018-12-07 16:14:43 -0400

    First Commit

---
+++ StoreApp/Models/StoreAppContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace StoreApp.Models
+{
+    public class StoreAppContext : DbContext, IStoreAppContext
+    {
+
+        public StoreAppContext() : base("name=StoreAppContext")
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public void MarkAsModified(Product item)
+        {
+            Entry(item).State = EntityState.Modified;
+        }
+    }
+}
